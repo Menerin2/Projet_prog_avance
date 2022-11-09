42e959cbb21e26085381ce5f11f5d63d874e69bc
kehrial <louis.maire5@gmail.com>
2022-11-09 16:03:43 +0100

    premiere fenetre buggée

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <stdbool.h>
 int main(int argc, char* argv[]){
+    SDL_Window* fenetre;
+    SDL_Event events;
+    bool terminer = false;
+    fenetre = SDL_CreateWindow("Pr Avancé", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* ecran = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED);
+    while(!terminer){
+        SDL_RenderClear(ecran);
+        SDL_PollEvent(&events);
+        switch(events.type){
+            case SDL_QUIT:
+                terminer = true;
+                break;
+        }
+        SDL_RenderPresent(ecran);
+    }
+    SDL_DestroyRenderer(ecran);
+    SDL_DestroyWindow(fenetre);
+    SDL_Quit();
     printf("camarche\n");
-    
+    return 0;
 }
