5a63b5c7cac50957c58269ad4e001e9a3870f837
kehrial <louis.maire5@gmail.com>
2022-12-20 19:45:45 +0100

    size de player_t useless

--- include/player.h
+++ include/player.h
@@ -4,8 +4,6 @@
 struct s_player;
 typedef struct s_player {
     /* data player */
-    int sizex;
-    int sizey;
     SDL_Texture* sprite;
     SDL_Rect* src_sprite;
 } player_t;
--- src/player.c
+++ src/player.c
@@ -2,8 +2,6 @@
 
 player_t* create_player(SDL_Renderer* renderer){
     player_t* dino = malloc(sizeof(player_t));
-    dino->sizex = 50;
-    dino->sizey = 50;
     dino->sprite = loadspritesdino(renderer);
     dino->src_sprite = loadspritesposition();
     return dino;
